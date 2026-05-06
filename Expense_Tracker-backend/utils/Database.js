ea68832817f1335b9dcd934c2235fd0eecd0a63a
hafidh <hafinhoramsey@gmail.com>
2026-05-06 17:13:03 +0300

    fix orm

--- Expense_Tracker-backend/utils/Database.js
+++ Expense_Tracker-backend/utils/Database.js
@@ -1,14 +1,14 @@
-const sequalize = require("sequelize");
+const { Sequelize } = require("sequelize");  
 require("dotenv").config();
 
-const db_connection = new sequalize(
-  process.env.DB_USER,
-  process.env.DB_PASSWD,
-  process.env.DB_USERNAME,
+const db_connection = new Sequelize(  
+  process.env.DB_NAME,      
+  process.env.DB_USERNAME,  
+  process.env.DB_PASSWD, 
   {
     dialect: "mysql",
     host: process.env.DB_HOST,
-  },
+  }
 );
 
 module.exports = db_connection;
